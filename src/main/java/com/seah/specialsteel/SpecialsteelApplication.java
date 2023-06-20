4e27160b45407cfa91e0bd34d74e7b38e1fc669c
jeong <deas124@gmail.com>
2023-06-20 16:44:13 +0900

    Initial commit

---
+++ src/main/java/com/seah/specialsteel/SpecialsteelApplication.java
@@ -0,0 +1,13 @@
+package com.seah.specialsteel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialsteelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialsteelApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/seah/specialsteel/SpecialsteelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.seah.specialsteel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpecialsteelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
