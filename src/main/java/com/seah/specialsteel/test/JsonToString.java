2cc5731058b71b686609b5b50e42c0251d277217
HalroWorld <halro92@gmail.com>
2023-07-05 17:32:15 +0900

    SONParser

--- src/main/java/com/seah/specialsteel/test/JsonToString.java
+++ src/main/java/com/seah/specialsteel/test/JsonToString.java
@@ -5,10 +5,9 @@ import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
 import org.json.simple.parser.ParseException;
 
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.Reader;
+import java.io.*;
+import java.math.BigDecimal;
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -19,40 +18,28 @@ public class JsonToString {
 
         JSONParser parser = new JSONParser();
 
-        Reader reader = new FileReader("C:\\jeongTest\\ResponseData_N29790.json");
+        FileInputStream fileInputStream = new FileInputStream("C:/path/ResponseData_N29790.json");
+        InputStreamReader reader = new InputStreamReader(fileInputStream, StandardCharsets.UTF_8);
+
 
         JSONObject jsonObject = (JSONObject) parser.parse(reader);
+//        System.out.println(jsonObject.toJSONString());
 
-        double totalCost = (Double) jsonObject.get("합금철_총_투입비용");
-        double totalAmount = (Double) jsonObject.get("합금철_총_투입량");
+        System.out.println(jsonObject.toJSONString());
 
         JSONArray jsonArray = (JSONArray) jsonObject.get("합금철별_투입량");
 
-        double expectOutput = (Double) jsonObject.get("예상_용강량");
-        Object expectMaterial = jsonObject.get("result_예상_성분");
-        String method = (String) jsonObject.get("방법");
 
-        System.out.println("합금철 총 투입비용 : " + totalCost);
-        System.out.println("합금철 총 투입량 : " + totalAmount);
-//
-//        List<Map<String, Object>> list = JsonUtils.getListMapFromJsonArray(jsonArray);
+        Map<String, BigDecimal> map = new HashMap<>();
+        for (int i = 0; i < jsonArray.size(); i++) {
+            JSONArray keyValueArray = (JSONArray) jsonArray.get(i);
+            String key = keyValueArray.get(0).toString();
 
-//        System.out.println(list.get(0).toString());
+            BigDecimal number = new BigDecimal(String.valueOf(keyValueArray.get(1)));
 
-
-//
-//        JSONObject testobj = new JSONObject();
-//        jsonArray.
-
-
-        for(int i=0; i< jsonArray.size(); i++){
-            JSONObject testobj =  (JSONObject) jsonArray.get(i);
-            String name = testobj.toString();
-
+            map.put(key, number);
+            System.out.println(key);
+            System.out.println(map.get(key));
         }
-//        System.out.println(input.getClass());
-//        for(Map<String, Object> input : inputs)
-//            System.out.println(input.toString());
-
     }
 }
