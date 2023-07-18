2b659153b8e87b79322220dce58f891bef133feb
이상봉 <120613827+HalroWorld@users.noreply.github.com>
2023-07-18 15:34:43 +0900

    짜잘한거 수정

--- src/main/java/com/seah/specialsteel/controller/MainController.java
+++ src/main/java/com/seah/specialsteel/controller/MainController.java
@@ -5,12 +5,14 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import static com.seah.specialsteel.controller.UploadController.oriResultDTO;
+
 @Controller
 public class MainController {
 
     @GetMapping({"/", "/result"})
     public String main(Model model) {
-
+        oriResultDTO = null;
         return "result";
     }
 
