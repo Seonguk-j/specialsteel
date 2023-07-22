7a297479bc994e936ca55e36ccf29ab2bdc2eefd
gusdlf5055 <131943487+gusdlf5055@users.noreply.github.com>
2023-07-22 16:04:09 +0900

    토요일(16:05)

--- src/main/java/com/seah/specialsteel/controller/DataController.java
+++ src/main/java/com/seah/specialsteel/controller/DataController.java
@@ -1,5 +1,6 @@
 package com.seah.specialsteel.controller;
 
+import com.seah.specialsteel.dto.CompareDTO;
 import com.seah.specialsteel.dto.DataDTO;
 import com.seah.specialsteel.dto.DateRequestDTO;
 import com.seah.specialsteel.dto.ResultDTO;
@@ -11,14 +12,11 @@ import com.seah.specialsteel.repository.AlloyInputRepository;
 import com.seah.specialsteel.repository.ExpectedResultRepository;
 import com.seah.specialsteel.repository.OriResultRepository;
 import com.seah.specialsteel.repository.RevResultRepository;
-import lombok.AllArgsConstructor;
-import lombok.extern.log4j.Log4j2;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDateTime;
 import java.util.*;
-import static com.seah.specialsteel.controller.UploadController.revResultDTOList;
 
 @RestController
 @RequestMapping("/api")
@@ -29,15 +27,20 @@ public class DataController {
     private final AlloyInputRepository alloyInputRepository;
     private final ExpectedResultRepository expectedResultRepository;
 
+
+
     public DataController(OriResultRepository oriResultRepository,
                           RevResultRepository revResultRepository,
                           AlloyInputRepository alloyInputRepository,
-                          ExpectedResultRepository expectedResultRepository) {
+                          ExpectedResultRepository expectedResultRepository, List<ResultDTO> oriResultDTOList) {
         this.oriResultRepository = oriResultRepository;
         this.revResultRepository = revResultRepository;
         this.alloyInputRepository = alloyInputRepository;
         this.expectedResultRepository = expectedResultRepository;
+
     }
+
+    private List<RevResult> revResults1;
 
     @PostMapping("/getDataByDate")
     public List<DataDTO> getDataByDate(@RequestBody DateRequestDTO dateRequest) {
@@ -54,8 +57,8 @@ public class DataController {
         // oriResults의 각 ID를 사용하여 RevResult를 조회하고 개수를 세어줍니다.
         for (OriResult oriResult : oriResults) {
             Long oriResultId = oriResult.getId();
-            List<RevResult> revResults = revResultRepository.findByOriResultId(oriResultId);
-            int revResultCount = revResults.size();
+            revResults1 = revResultRepository.findByOriResultId(oriResultId);
+            int revResultCount = revResults1.size();
 
             // DataDTO 객체를 생성하여 원하는 정보를 추가합니다.
             DataDTO dataDTO = new DataDTO();
@@ -71,180 +74,135 @@ public class DataController {
         return resultData;
     }
 
-
+    private List<ResultDTO> oriResultDTOList = new ArrayList<>();
+    private List<RevResult> revResults;
+    private List<ResultDTO> revResultDTOList = new ArrayList<>();
+    private Map<String, Object> resultMap = new HashMap<>();
 
     @GetMapping("/getDataById/{id}")
     public Map<String, Object> getDataById(@PathVariable Long id) {
-        Map<String, Object> resultMap = new HashMap<>();
-
         // RevResult 조회
-        List<RevResult> revResults = revResultRepository.findByOriResultId(id);
-        for (RevResult revResult : revResults) {
-            ResultDTO resultDTO = revResult.entityToDTO();
+        revResults = revResultRepository.findByOriResultId(id);
+        Optional<OriResult> oriResults = oriResultRepository.findById(id);
 
-            List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(revResult.getId());
-            LinkedHashMap<String, String> alloyHashMap = new LinkedHashMap<>();
+        System.out.println(revResults);
+        System.out.println(oriResults);
+        System.out.println("리브리설트디티오사이즈 - "+revResultDTOList.size());
 
-            for(AlloyInput alloyInput : alloyInputs) {
-                alloyHashMap.put(alloyInput.getName(), alloyInput.getAmount()+"");
-            }
-            resultDTO.setAlloyInputs(alloyHashMap);
-
-            List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(revResult.getId());
-            LinkedHashMap<String, String> expectHashMap = new LinkedHashMap<>();
-
-            for(ExpectedResult expectedResult : expectedResults) {
-                expectHashMap.put(expectedResult.getName(), expectedResult.getAmount()+"");
-            }
-            resultDTO.setExpectMaterials(expectHashMap);
-            revResultDTOList.add(resultDTO);
-        }
-
-
-        OriResult oriResult = oriResultRepository.findById(id).orElseThrow();
-
-        ResultDTO oriResultDTO = oriResult.entityToDTO();
-
-        List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(oriResult.getId());
-
-        LinkedHashMap<String, String> alloyHashMap = new LinkedHashMap<>();
-        for(AlloyInput alloyInput : alloyInputs) {
-            alloyHashMap.put(alloyInput.getName(), alloyInput.getAmount()+"");
-        }
-        oriResultDTO.setAlloyInputs(alloyHashMap);
-
-        List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(oriResult.getId());
-        LinkedHashMap<String, String> expectHashMap = new LinkedHashMap<>();
-
-        for(ExpectedResult expectedResult : expectedResults) {
-            expectHashMap.put(expectedResult.getName(), expectedResult.getAmount()+"");
-        }
-        oriResultDTO.setExpectMaterials(expectHashMap);
-
-        //기존 알고리즘 넣기
-        resultMap.put("oriResults",oriResult);
         if (!revResults.isEmpty()) {
-            //수정 알고리즘 넣기
-            resultMap.put("revResults", revResultDTOList); // 모든 RevResult를 담는 List로 수정
+
+            for (RevResult revResult : revResults) {
+                ResultDTO resultDTO = new ResultDTO();
+                resultDTO.id = revResult.getId(); // Assuming there is a getId() method in RevResult class
+                resultDTO.totalCost = revResult.getTotalCost(); // Assuming there is a getTotalCost() method in RevResult class
+                resultDTO.method = revResult.getMethod();
+                resultDTO.comment = revResult.getComment();
+                resultDTO.totalAmount = revResult.getTotalAmount();
+                resultDTO.expectOutput = revResult.getExpectOutput();
+
+
+                // AlloyInput 조회
+                List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(revResult.getId());
+                Map<String, String> alloyInputsMap = new LinkedHashMap<>();
+                for (AlloyInput alloyInput : alloyInputs) {
+                    // Assuming there is a getName() method and getValue() method in AlloyInput class
+                    alloyInputsMap.put(alloyInput.getName(), String.valueOf(alloyInput.getAmount()));
+                }
+                resultDTO.alloyInputs = (LinkedHashMap<String, String>) alloyInputsMap;
+
+                // ExpectedResult 조회
+                List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(revResult.getId());
+                Map<String, String> expectedMaterialsMap = new LinkedHashMap<>();
+                for (ExpectedResult expectedResult : expectedResults) {
+                    // Assuming there is a getMaterialName() method and getValue() method in ExpectedResult class
+                    expectedMaterialsMap.put(expectedResult.getName(), String.valueOf(expectedResult.getAmount()));
+                }
+                resultDTO.expectMaterials = (LinkedHashMap<String, String>) expectedMaterialsMap;
+
+                revResultDTOList.add(resultDTO);
+                System.out.println("리브리썰트 - "+revResultDTOList);
+            }
+        }
+
+        if (oriResults.isPresent()) {
+            System.out.println("오리리썰트이프들어옴");
+            ResultDTO oriresultDTO = new ResultDTO();
+            oriresultDTO.id = oriResults.get().getId(); // Assuming there is a getId() method in RevResult class
+            oriresultDTO.totalCost = oriResults.get().getTotalCost(); // Assuming there is a getTotalCost() method in RevResult class
+            oriresultDTO.totalAmount = oriResults.get().getTotalAmount();
+            oriresultDTO.comment = oriResults.get().getComment();
+            oriresultDTO.method = oriResults.get().getMethod();
+            oriresultDTO.expectOutput = oriResults.get().getExpectOutput();
+            System.out.println("오리아이디 - "+oriResults.get().getId());
             // AlloyInput 조회
-//            List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(id);
-//            resultMap.put("alloyInputs", alloyInputs);
-//
-//            // ExpectedResult 조회
-//            List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(id);
-//            resultMap.put("expectedResults", expectedResults);
+            List<AlloyInput> alloyInputs = alloyInputRepository.findByOriResultId(oriResults.get().getId());
+            Map<String, String> alloyInputsMap = new LinkedHashMap<>();
+            for (AlloyInput alloyInput : alloyInputs) {
+                // Assuming there is a getName() method and getValue() method in AlloyInput class
+                alloyInputsMap.put(alloyInput.getName(), String.valueOf(alloyInput.getAmount()));
+            }
+            System.out.println("오리얼로이맵 - "+alloyInputsMap);
+            oriresultDTO.alloyInputs = (LinkedHashMap<String, String>) alloyInputsMap;
+
+            // ExpectedResult 조회
+            List<ExpectedResult> expectedResults = expectedResultRepository.findByOriResultId(oriResults.get().getId());
+            Map<String, String> expectedMaterialsMap1 = new LinkedHashMap<>();
+            for (ExpectedResult expectedResult : expectedResults) {
+                expectedMaterialsMap1.put(expectedResult.getName(), String.valueOf(expectedResult.getAmount()));
+            }
+            oriresultDTO.expectMaterials = (LinkedHashMap<String, String>) expectedMaterialsMap1;
+            System.out.println("오리여기까지");
+            oriResultDTOList.add(oriresultDTO);
+            System.out.println("오리리설트 - "+oriResultDTOList);
+        }
+
+
+        System.out.println("------------------여기5----------------");
+        System.out.println(revResults);
+        System.out.println("------------------여기5----------------");
+        resultMap.put("oriResults",oriResults);
+        if (!revResults.isEmpty()) {
+            resultMap.put("revResults", revResults); // 모든 RevResult를 담는 List로 수정
+            // AlloyInput 조회
+            List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(id);
+            resultMap.put("alloyInputs", alloyInputs);
+
+            // ExpectedResult 조회
+            List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(id);
+            resultMap.put("expectedResults", expectedResults);
         } else {
             resultMap.put("error", "RevResult not found with ID: " + id);
         }
-        System.out.println(resultMap.toString());
+        System.out.println("------------------여기6----------------");
+        System.out.println(resultMap);
+        System.out.println("------------------여기6----------------");
         return resultMap;
     }
 
-//    @PostMapping("/getDataByDate")
-//    public List<DataDTO> getDataByDate(@RequestBody DateRequestDTO dateRequest) {
-//        // 클라이언트에서 전달한 startDate와 endDate를 이용하여 데이터를 조회합니다.
-//        LocalDateTime startDate = dateRequest.getStartDate();
-//        LocalDateTime endDate = dateRequest.getEndDate();
-//        System.out.println("----------------------------------");
-//        System.out.println(startDate);
-//        System.out.println(endDate);
-//        System.out.println("----------------------------------");
-//
-//        // 조회에 필요한 작업을 수행합니다.
-//        List<OriResult> oriResults = oriResultRepository.findByModDateBetween(startDate, endDate);
-//        List<RevResult> revResults = revResultRepository.findByModDateBetween(startDate, endDate);
-//
-//        System.out.println("---------------여기-----------------");
-//        System.out.println(oriResults);
-//        System.out.println(revResults);
-//        System.out.println("---------------여기-----------------");
-//
-//        // 결과를 저장할 리스트를 초기화합니다.
-//        List<DataDTO> resultData = new ArrayList<>();
-//
-//        // OriResult를 기준으로 데이터를 조회합니다.
-//        for (OriResult oriResult : oriResults) {
-//            Long oriResultId = oriResult.getId();
-//            System.out.println("---------------여기1-----------------");
-//            System.out.println(oriResultId);
-//            System.out.println("---------------여기1-----------------");
-//            // OriResult에 해당하는 AlloyInput을 조회합니다.
-//            List<AlloyInput> alloyInputs = alloyInputRepository.findByOriResultId(oriResultId);
-//            System.out.println("---------------여기2-----------------");
-//            System.out.println(alloyInputs);
-//            System.out.println("---------------여기2-----------------");
-//            // OriResult에 해당하는 ExpectedResult를 조회합니다.
-//            List<ExpectedResult> expectedResults = expectedResultRepository.findByOriResultId(oriResultId);
-//            System.out.println("---------------여기3-----------------");
-//            System.out.println(expectedResults);
-//            System.out.println("---------------여기3-----------------");
-//            // 데이터를 DataDTO로 변환하고 결과 리스트에 추가합니다.
-//            List<DataDTO> dataDTOList = convertToDataDTO(oriResult, null, alloyInputs, expectedResults);
-//            System.out.println("---------------여기4-----------------");
-//            System.out.println(dataDTOList);
-//            System.out.println("---------------여기4-----------------");
-//            resultData.addAll(dataDTOList);
-//        }
-//
-//        // RevResult를 기준으로 데이터를 조회합니다.
-//        for (RevResult revResult : revResults) {
-//            Long revResultId = revResult.getId();
-//            // RevResult에 해당하는 AlloyInput을 조회합니다.
-//            List<AlloyInput> alloyInputs = alloyInputRepository.findByRevResultId(revResultId);
-//
-//            // RevResult에 해당하는 ExpectedResult를 조회합니다.
-//            List<ExpectedResult> expectedResults = expectedResultRepository.findByRevResultId(revResultId);
-//
-//            // RevResult에 해당하는 OriResult를 조회합니다.
-//            OriResult oriResult = revResult.getOriResult();
-//
-//            // 데이터를 DataDTO로 변환하고 결과 리스트에 추가합니다.
-//            List<DataDTO> dataDTOList = convertToDataDTO(null, revResult, alloyInputs, expectedResults);
-//            resultData.addAll(dataDTOList);
-//        }
-//
-//        return resultData;
-//    }
+    @PostMapping("/getRevData")
+    public ResponseEntity<Map<String, Object>> getRevData(@RequestParam ("index")int index) {
+        System.out.println("리브리설트디티오사이즈 - " + revResultDTOList.size());
+        System.out.println(oriResultDTOList.get(0));
+        revResultDTOList.get(index).setLength(revResults.size());
+        ResultDTO revResultDTO = revResultDTOList.get(index);
+        ResultDTO oriResultDTO = oriResultDTOList.get(0);
 
-    // 데이터를 DTO로 변환하는 메서드 (원하는 형식으로 변환하시면 됩니다)
-    private List<DataDTO> convertToDataDTO(OriResult oriResult, RevResult revResult, List<AlloyInput> alloyInputs, List<ExpectedResult> expectedResults) {
-        // 데이터를 변환하여 DataDTO 리스트로 생성합니다.
-        List<DataDTO> dataDTOList = new ArrayList<>();
+        if (oriResultDTOList != null) {
 
-        // 예시로 OriResult와 RevResult 데이터를 활용하여 DataDTO를 생성하는 로직을 추가합니다.
-        // OriResult와 RevResult의 구체적인 필드와 DataDTO의 필드를 매핑하여 설정해야합니다.
-        // 예시로 dataDTO.setName(...)과 dataDTO.setAmount(...)에 적절한 데이터를 설정합니다.
-        if (oriResult != null) {
-            DataDTO dataDTO = new DataDTO();
-            dataDTO.setDate(oriResult.getModDate()); // 예시로 name 필드를 가져옴
-            dataDTO.setComment(oriResult.getComment()); // 예시로 amount 필드를 가져옴
-            dataDTOList.add(dataDTO);
+            CompareDTO compareDTO = new CompareDTO(oriResultDTO, revResultDTO);
+
+            Map<String, Object> response = new HashMap<>();
+            response.put("revResultDTO", revResultDTO);
+            response.put("compareDTO", compareDTO);
+            System.out.println("리스폰스 - "+ response);
+            return ResponseEntity.ok(response);
+        } else {
+            return ResponseEntity.notFound().build();
         }
 
-        if (revResult != null) {
-            DataDTO dataDTO = new DataDTO();
-            dataDTO.setDate(revResult.getModDate()); // 예시로 name 필드를 가져옴
-            dataDTO.setComment(revResult.getComment()); // 예시로 amount 필드를 가져옴
-            dataDTOList.add(dataDTO);
-        }
 
-        // 예시로 AlloyInput 데이터를 활용하여 DataDTO를 생성하는 로직을 추가합니다.
-        for (AlloyInput alloyInput : alloyInputs) {
-            DataDTO dataDTO = new DataDTO();
-            dataDTO.setName(alloyInput.getName());
-            dataDTO.setAmount(alloyInput.getAmount());
-            dataDTOList.add(dataDTO);
-        }
-
-        // 예시로 ExpectedResult 데이터를 활용하여 DataDTO를 생성하는 로직을 추가합니다.
-        for (ExpectedResult expectedResult : expectedResults) {
-            DataDTO dataDTO = new DataDTO();
-            dataDTO.setName(expectedResult.getName());
-            dataDTO.setAmount(expectedResult.getAmount());
-            dataDTOList.add(dataDTO);
-        }
-
-        // 변환된 DataDTO 리스트를 반환합니다.
-        return dataDTOList;
     }
+
+
+
 }
